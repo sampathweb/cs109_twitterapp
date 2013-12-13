d19f5e7a8bd3612b8175ae9681fc3f81b3dbc9d4
Ramesh Sampath <ramesh@sampathweb.com>
2013-12-12 20:03:27 -0600

    update

--- app/twitterword.py
+++ app/twitterword.py
@@ -21,26 +21,12 @@ import scipy as sp
 import matplotlib.pyplot as plt
 import pandas as pd
 from matplotlib import rcParams
-import matplotlib.cm as cm
-
-#Specific for what is used below
-# import oauth2 as oauth
-import urlparse
-import requests
-import csv
-from pattern import web
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.cross_validation import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 
 from app.helpers import get_words_df
-
-def main():
-    pass
-
-if __name__ == '__main__':
-    main()
 
 def make_xy(df, vectorizer=None):
     #Make bag of words, X is the count array, y is the columns
@@ -60,14 +46,13 @@ def log_likelihood(clf, x, y):
 
 def recommend(twitterword):
     newpd = get_words_df()
-    # newpd = pd.read_csv('twitter_bigdf_appended_cleanedtweets_averageperuser.csv')
+    #newpd = pd.read_csv('twitter_bigdf_appended_cleanedtweets_averageperuser.csv')
     newpd['Tweet'] = newpd['Tweet'].map(lambda x: str(x))
 
     newpd['was_retweeted'] = newpd['average_retweet_threshold']
 
     best_alpha = 50.0
     best_min_df = 0.01
-
 
     vectorizer = CountVectorizer(min_df=best_min_df)
     x, y = make_xy(newpd, vectorizer)
@@ -80,11 +65,9 @@ def recommend(twitterword):
     prob = clf.predict_proba(x)[:, 0]
     predict = clf.predict(x)
 
-    retweet_chance = abs(clf.predict_proba(vectorizer.transform([twitterword])) / probs.mean())
+    retweet_chance = clf.predict_proba(vectorizer.transform([twitterword]))
 
-    answer = 'Compared to an average tweet, using "%s" in your tweet has the likliehood of a retweet of '%twitterword + str(retweet_chance[0][1])
-
+    answer = retweet_chance[0][1] * 100
     return answer
-
 
 
